8663a7373a1f44aafc6058c9d0bf0bc73ea6b7a4
Burcu Dogan <burcujdogan@gmail.com>
2012-03-20 11:56:07 +0200

    Adding cmp flag to Type during json export.

--- src/net/sourceforge/plantuml/jsonexporter/Options.java
+++ src/net/sourceforge/plantuml/jsonexporter/Options.java
@@ -3,8 +3,12 @@ package net.sourceforge.plantuml.jsonexporter;
 public class Options {
 	
 	final public static String KEY_ARRAY = "array";
+	
 	final public static String[] OUTPUT_VISIBILITIES = 
 			new String[]{"public", "private", "protected", "package"};
+	// TODO: allow overriding of this setting via file input
+	final public static String[] OUTPUT_PRIMITIVES = 
+			new String[]{"number", "boolean", "object", "string"};
 	
 	public static String FILES_OUPUT_DIRECTORY = "json/";
 
--- src/net/sourceforge/plantuml/jsonexporter/models/Type.java
+++ src/net/sourceforge/plantuml/jsonexporter/models/Type.java
@@ -1,16 +1,30 @@
 package net.sourceforge.plantuml.jsonexporter.models;
 
+import java.util.Arrays;
+
+import net.sourceforge.plantuml.jsonexporter.Options;
 
 public class Type {
-	
+
 	protected String name;
 	protected boolean array;
 	protected boolean cmp;
-	
-	public Type(String signature){
+
+	/**
+	 * Instantiates a new type.
+	 * 
+	 * @param signature
+	 */
+	public Type(String signature) {
+		// TODO: move array rep string/regex to options?
 		this.name = signature.replaceAll("\\[\\]", "");
 		this.array = signature.indexOf("[]") > 0;
-		
-		// TODO: find if cmp or not
+
+		// search if type is primitive
+		// or not
+		this.cmp = !(Arrays
+			.asList(Options.OUTPUT_PRIMITIVES)
+			.contains(this.name.toLowerCase()));
+
 	}
 }
